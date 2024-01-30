fb371711fcc711fd5069b113c5155bd534c1cb13
ctomrp <cristian.rico.palma@gmail.com>
2024-01-30 19:10:05 -0300

    loader

--- script/loader.js
+++ script/loader.js
@@ -1,6 +1,10 @@
 window.addEventListener('load', function() {
+    this.document.querySelector(".body").style.display = 'none';        
+   
+    
     /* quitar el setTimeout en prod */
     setTimeout(() => {
         this.document.querySelector(".loader").style.display = 'none';        
+        this.document.querySelector(".body").style.display = 'block';
     }, 3000);
 });

dd003707e266dd5f8c7592dad79a4ecd06d9a780
ctomrp <cristian.rico.palma@gmail.com>
2024-01-31 16:53:57 -0300

    fixes

--- script/loader.js
+++ script/loader.js
@@ -1,10 +1,8 @@
-window.addEventListener('load', function() {
-    this.document.querySelector(".body").style.display = 'none';        
-   
-    
-    /* quitar el setTimeout en prod */
+window.addEventListener('load', function () {
+    this.document.querySelector(".body").style.display = 'none';
+
     setTimeout(() => {
-        this.document.querySelector(".loader").style.display = 'none';        
+        this.document.querySelector(".loader").style.display = 'none';
         this.document.querySelector(".body").style.display = 'block';
     }, 3000);
 });
